42f72d4ebd4ce2d7bd7b18f7f256357303c12067
erza <58721335+erzaffasya@users.noreply.github.com>
2022-03-02 12:12:38 +0700

    migration

--- database/migrations/2022_02_18_140205_create_laporans_table.php
+++ database/migrations/2022_02_18_140205_create_laporans_table.php
@@ -22,11 +22,11 @@ return new class extends Migration
             $table->longText('jumat')->nullable();
             $table->longText('mingguan')->nullable();
             $table->longText('komentar')->nullable();
-            $table->integer('isVerif')->nullable();
+            $table->integer('isVerif')->default(4);
             // Ketentuan isVerif
             // 1 = udah ga revisi atau approve
             // 0 = revisi
-            // NULL = baru ke create
+            // 4 = baru ke create
             // 2 = mahasiswa ngirim revisi
             $table->foreignId("user_id")->constrained("users")->onDelete("cascade")->onUpdate("cascade");
             $table->foreignId("divisi_id")->constrained("divisi")->onDelete("cascade")->onUpdate("cascade");
