841e488853e6dccce0b28fc65c51d15f1edd06c3
jihanapriliani <11211046@student.itk.ac.id>
2024-01-09 21:02:41 +0800

    fix pekerjaan teknis form

--- resources/views/admin/anggota/penilaian.blade.php
+++ resources/views/admin/anggota/penilaian.blade.php
@@ -29,8 +29,8 @@
                 <div class="card-body px-3 pt-0">
                     <form method="POST" action="storeUser">
                         @csrf
-                        <p class="text-sm fw-bold my-3">Masukkan Nilai</p>
-                        <div class="mb-3 d-flex">
+                        <p class="text-sm fw-bold my-3">Poin Penilaian</p>
+                        <!-- <div class="mb-3 d-flex">
                             <span class="text-sm fw-bold" style="width: 5%">1. </span>
                             <label for="exampleFormControlSelect1" style="width: 80%" class="me-2">Mengoperasikan paket program pengolah angka (spreadsheet/ excel)</label>
                             <div style="width:15%">
@@ -41,8 +41,8 @@
                                 {{-- <span class="text-xxs mt-n4 p-0 text-warning fw-bold">Kurang</span> --}}
                                 {{-- <span class="text-xxs mt-n4 p-0 text-danger fw-bold">Sangat Kurang</span> --}}
                             </div>
-                        </div>
-                        <div class="mb-3 d-flex">
+                        </div> -->
+                        <!-- <div class="mb-3 d-flex">
                             <span class="text-sm fw-bold" style="width: 5%">2. </span>
                             <label for="exampleFormControlSelect1" style="width: 80%" class="me-2">Mengoperasikan aplikasi pengolah kata (mc. word)</label>
                             <div style="width:15%">
@@ -89,7 +89,107 @@
                                 {{-- <span class="text-xxs mt-n4 p-0 text-warning fw-bold">Kurang</span> --}}
                                 {{-- <span class="text-xxs mt-n4 p-0 text-danger fw-bold">Sangat Kurang</span> --}}
                             </div>
+                        </div> -->
+
+                        
+                       
+
+                        <div class=" align-items-center row my-3">
+                            <div class="col-lg-9 col-sm-3 my-1">
+                                <label class="sr-only" for="inlineFormInputName">Name</label>
+                                <input type="text" class="form-control" id="inlineFormInputName" placeholder="Masukkan Poin Penilaian">
+                                <p style="font-size: 0.7rem;">cth: Mengoperasikan paket program pengolah angka (spreadsheet/ excel) </p>
+                            </div>
+                            <div class="col-lg-2 col-sm-3 my-1">
+                                <label class="sr-only" for="inlineFormInputGroupUsername">Nilai</label>
+                                <div class="input-group">
+                                    <input type="number" class="form-control" name="nilai2" id="inlineFormInputGroupUsername" placeholder="Nilai">
+                                </div>
+                                <p style="font-size: 0.7rem;">cth: 86</p>
+                            </div>
+                      
                         </div>
+
+
+
+                        <div class=" align-items-center row my-3">
+                            <div class="col-lg-9 col-sm-3 my-1">
+                                <label class="sr-only" for="inlineFormInputName">Name</label>
+                                <input type="text" class="form-control" id="inlineFormInputName" placeholder="Masukkan Poin Penilaian">
+                                <p style="font-size: 0.7rem;">cth: Mengoperasikan paket program pengolah angka (spreadsheet/ excel) </p>
+                            </div>
+                            <div class="col-lg-2 col-sm-3 my-1">
+                                <label class="sr-only" for="inlineFormInputGroupUsername">Nilai</label>
+                                <div class="input-group">
+                                    <input type="number" class="form-control" name="nilai2" id="inlineFormInputGroupUsername" placeholder="Nilai">
+                                </div>
+                                <p style="font-size: 0.7rem;">cth: 86</p>
+                            </div>
+                            
+                         
+                        </div>
+
+                        <div class=" align-items-center row my-3">
+                            <div class="col-lg-9 col-sm-3 my-1">
+                                <label class="sr-only" for="inlineFormInputName">Name</label>
+                                <input type="text" class="form-control" id="inlineFormInputName" placeholder="Masukkan Poin Penilaian">
+                                <p style="font-size: 0.7rem;">cth: Mengoperasikan paket program pengolah angka (spreadsheet/ excel) </p>
+                            </div>
+                            <div class="col-lg-2 col-sm-3 my-1">
+                                <label class="sr-only" for="inlineFormInputGroupUsername">Nilai</label>
+                                <div class="input-group">
+                                    <input type="number" class="form-control" name="nilai3" id="inlineFormInputGroupUsername" placeholder="Nilai">
+                                </div>
+                                <p style="font-size: 0.7rem;">cth: 86</p>
+                            </div>
+                            
+                            
+                        </div>
+
+                        <div class=" align-items-center row my-3">
+                            <div class="col-lg-9 col-sm-3 my-1">
+                                <label class="sr-only" for="inlineFormInputName">Name</label>
+                                <input type="text" class="form-control" id="inlineFormInputName" placeholder="Masukkan Poin Penilaian">
+                                <p style="font-size: 0.7rem;">cth: Mengoperasikan paket program pengolah angka (spreadsheet/ excel) </p>
+                            </div>
+                            <div class="col-lg-2 col-sm-3 my-1">
+                                <label class="sr-only" for="inlineFormInputGroupUsername">Nilai</label>
+                                <div class="input-group">
+                                    <input type="number" class="form-control" name="nilai4" id="inlineFormInputGroupUsername" placeholder="Nilai">
+                                </div>
+                                <p style="font-size: 0.7rem;">cth: 86</p>
+                            </div>
+                            
+                           
+                        </div>
+
+
+                        <div class=" align-items-center row my-3">
+                            <div class="col-lg-9 col-sm-3 my-1">
+                                <label class="sr-only" for="inlineFormInputName">Name</label>
+                                <input type="text" class="form-control" id="inlineFormInputName" placeholder="Masukkan Poin Penilaian">
+                                <p style="font-size: 0.7rem;">cth: Mengoperasikan paket program pengolah angka (spreadsheet/ excel) </p>
+                            </div>
+                            <div class="col-lg-2 col-sm-3 my-1">
+                                <label class="sr-only" for="inlineFormInputGroupUsername">Nilai</label>
+                                <div class="input-group">
+                                    <input type="number" class="form-control" name="nilai5" id="inlineFormInputGroupUsername" placeholder="Nilai">
+                                </div>
+                                <p style="font-size: 0.7rem;">cth: 86</p>
+                            </div>
+                            
+                           
+                        </div>
+
+
+                      
+
+
+                      
+
+                     
+
+
                         
                         {{-- <button type="submit" class="btn mb-0 bg-gradient-primary">Submit!</button> --}}
                     </form>
@@ -105,12 +205,14 @@
                 <div class="card-body px-3 pt-0">
                     <form method="POST" action="storeUser">
                         @csrf
-                        <p class="text-sm fw-bold my-3">Masukkan Nilai</p>
+                        <p class="text-sm fw-bold my-3">Poin Penilaian</p>
                         <div class="mb-3 d-flex">
                             <span class="text-sm fw-bold" style="width: 5%">1. </span>
                             <label for="exampleFormControlSelect1" style="width: 70%" class="me-2">Disiplin</label>
                             <div style="width:25%">
-                                <input type="text" class="form-control-sm m-0" name="name" placeholder="nilai" style="width:100%; border: 1px solid lightgrey">
+                                <div class="input-group">
+                                    <input type="number" class="form-control" name="nilai6" id="inlineFormInputGroupUsername" placeholder="Nilai">
+                                </div>
                                 <span class="text-xxs mt-n4 p-0 text-success fw-bold">Sangat Baik</span>
                                 {{-- <span class="text-xxs mt-n4 p-0 text-success fw-bold">Baik</span> --}}
                                 {{-- <span class="text-xxs mt-n4 p-0 text-success fw-bold">Cukup</span> --}}
@@ -122,7 +224,9 @@
                             <span class="text-sm fw-bold" style="width: 5%">2. </span>
                             <label for="exampleFormControlSelect1" style="width: 70%" class="me-2">Tanggung Jawab</label>
                             <div style="width:25%">
-                                <input type="text" class="form-control-sm m-0" name="name" placeholder="nilai" style="width:100%; border: 1px solid lightgrey">
+                                 <div class="input-group">
+                                    <input type="number" class="form-control" name="nilai7" id="inlineFormInputGroupUsername" placeholder="Nilai">
+                                </div>
                                 <span class="text-xxs mt-n4 p-0 text-success fw-bold">Sangat Baik</span>
                                 {{-- <span class="text-xxs mt-n4 p-0 text-success fw-bold">Baik</span> --}}
                                 {{-- <span class="text-xxs mt-n4 p-0 text-success fw-bold">Cukup</span> --}}
@@ -134,7 +238,9 @@
                             <span class="text-sm fw-bold" style="width: 5%">3. </span>
                             <label for="exampleFormControlSelect1" style="width: 70%" class="me-2">Inisiatif</label>
                             <div style="width:25%">
-                                <input type="text" class="form-control-sm m-0" name="name" placeholder="nilai" style="width:100%; border: 1px solid lightgrey">
+                                <div class="input-group">
+                                    <input type="number" class="form-control" name="nilai8" id="inlineFormInputGroupUsername" placeholder="Nilai">
+                                </div>
                                 <span class="text-xxs mt-n4 p-0 text-success fw-bold">Sangat Baik</span>
                                 {{-- <span class="text-xxs mt-n4 p-0 text-success fw-bold">Baik</span> --}}
                                 {{-- <span class="text-xxs mt-n4 p-0 text-success fw-bold">Cukup</span> --}}
@@ -146,7 +252,9 @@
                             <span class="text-sm fw-bold" style="width: 5%">4. </span>
                             <label for="exampleFormControlSelect1" style="width: 70%" class="me-2">Kebersihan dan kerapihan</label>
                             <div style="width:25%">
-                                <input type="text" class="form-control-sm m-0" name="name" placeholder="nilai" style="width:100%; border: 1px solid lightgrey">
+                                <div class="input-group">
+                                    <input type="number" class="form-control" name="nilai9" id="inlineFormInputGroupUsername" placeholder="Nilai">
+                                </div>
                                 <span class="text-xxs mt-n4 p-0 text-success fw-bold">Sangat Baik</span>
                                 {{-- <span class="text-xxs mt-n4 p-0 text-success fw-bold">Baik</span> --}}
                                 {{-- <span class="text-xxs mt-n4 p-0 text-success fw-bold">Cukup</span> --}}
@@ -158,7 +266,9 @@
                             <span class="text-sm fw-bold" style="width: 5%">5. </span>
                             <label for="exampleFormControlSelect1" style="width: 70%" class="me-2">Keselamatan dan kesehatan kerja (K3)</label>
                             <div style="width:25%">
-                                <input type="text" class="form-control-sm m-0" name="name" placeholder="nilai" style="width:100%; border: 1px solid lightgrey">
+                                <div class="input-group">
+                                    <input type="number" class="form-control" name="nilai10" id="inlineFormInputGroupUsername" placeholder="Nilai">
+                                </div>
                                 <span class="text-xxs mt-n4 p-0 text-success fw-bold">Sangat Baik</span>
                                 {{-- <span class="text-xxs mt-n4 p-0 text-success fw-bold">Baik</span> --}}
                                 {{-- <span class="text-xxs mt-n4 p-0 text-success fw-bold">Cukup</span> --}}
@@ -186,7 +296,7 @@
                             </div>
                         </div>
                         <div>
-                            <button type="submit" class="btn mb-0 bg-gradient-primary">Submit!</button>
+                            <button type="submit" class="btn mb-0 bg-gradient-primary">Simpan!</button>
                         </div>
                     </div>
                 </div>
@@ -195,3 +305,33 @@
         
     </div>
 </x-app-layout>
+
+<script>
+    function editRow(id) {
+        var inputName = document.getElementById('inlineFormInputName');
+        var inputNilai = document.getElementById('inlineFormInputGroupUsername');
+        var button = document.querySelector('.btn-primary');
+
+        if (button.innerText === 'Simpan') {
+            // Set nilai input dan disabled
+            inputName.value = "Mengoperasikan paket program pengolah angka (spreadsheet/ excel)";
+            inputNilai.value = 86;
+            inputName.disabled = true;
+            inputNilai.disabled = true;
+
+            // Ganti teks tombol dan hapus atribut data-id
+            button.innerText = 'Perbarui';
+            button.removeAttribute('data-id');
+        } else {
+            // Reset nilai input dan disabled
+            inputName.value = "";
+            inputNilai.value = "";
+            inputName.disabled = false;
+            inputNilai.disabled = false;
+
+            // Ganti teks tombol dan tambahkan atribut data-id
+            button.innerText = 'Simpan';
+            button.setAttribute('data-id', id);
+        }
+    }
+</script>
