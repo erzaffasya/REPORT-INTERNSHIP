4212fc1642cafee98cef9881ab3db94f15e742be
jihanapriliani <11211046@student.itk.ac.id>
2024-01-15 23:32:12 +0800

    .

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -4,7 +4,7 @@
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
             aria-hidden="true" id="iconSidenav"></i>
         <a class="navbar-brand m-0 d-flex align-items-center"
-            href=" https://demos.creative-tim.com/soft-ui-dashboard-pro/pages/dashboards/default.html " target="_blank">
+            href="/" target="_blank">
             <img src="{{ asset('tadmin/assets/img/logo-ct-dark.png') }}" class="navbar-brand-img h-100"
                 alt="main_logo">
             <div class="ms-1">
