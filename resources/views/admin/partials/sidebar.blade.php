ffb96517fd25090900bde020534de24364bb0596
Erza Fahmi Fasya <58721335+erzaffasya@users.noreply.github.com>
2025-06-22 19:24:08 +0800

    Update sidebar.blade.php

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -6,9 +6,9 @@
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
             aria-hidden="true" id="iconSidenav"></i>
         <a class="navbar-brand m-0 d-flex align-items-center" href="/" target="_blank">
-            <img src="{{ asset('tadmin/assets/img/logo-ct-dark.png') }}" class="navbar-brand-img h-100" alt="main_logo">
+            <img src="{{ asset('tadmin/assets/img/pt-ajk.png') }}" class="navbar-brand-img h-100" alt="main_logo">
             <div class="ms-1">
-                <span class="ms-1 mb-0 font-weight-bold fs-6">PT AJK</span>
+                <span class="ms-1 mb-0 font-weight-bold fs-6">Monitoring System</span>
                 {{-- <div class="ms-1">PT AJK</div> --}}
             </div>
         </a>
